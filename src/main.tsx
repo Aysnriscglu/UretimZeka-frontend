c7965485ba568097f565c32235431e0b66af6677
Aysnriscglu <aiscioglu23@posta.pau.edu.tr>
2026-08-07 11:04:13 +0300

    Require login (and show video) on every page refresh

--- src/main.tsx
+++ src/main.tsx
@@ -5,6 +5,9 @@ import "./index.css";
 import App from "./App";
 import Login from "./pages/Login";
 
+// Sayfa her yenilendiğinde (F5) çıkış yapılmış sayılması için auth durumunu temizle
+localStorage.removeItem("isAuthenticated");
+
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
     <BrowserRouter>
